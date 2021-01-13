6c90d7360f4c1b65d9c964f7a2d002c90c280bb6
Calinemesef <calin_calinnemes@yahoo.com>
2021-01-13 13:24:02 +0200

    v5

---
+++ test/src/AufgabeTest.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AufgabeTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPrioritat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPrioritat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNotize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNotize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTeilaufgaben() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTeilaufgaben() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
---
+++ test/src/AufgabenListeTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class AufgabenListeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loschen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void einfugen() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void andern() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAlleAufgaben() {
+    }
+}
